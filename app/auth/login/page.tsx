e28c52423426020de958126d075e9cd0dd40627e
Tarun Rawat <144971861+tarun05rawat@users.noreply.github.com>
2024-12-31 00:07:57 +0400

    used router.replace to go back to landing page when clicked on logo

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -24,7 +24,7 @@ export default function LoginPage() {
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-background to-secondary p-4">
       <Card className="w-full max-w-md">
         <CardHeader>
-          <h1 className="text-2xl font-bold text-center">Welcome Back</h1>
+          <h1 className="text-2xl font-bold text-center">Welcome</h1>
         </CardHeader>
         <CardContent>
           <Button
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,12 +2,23 @@
 
 import { signOut } from "next-auth/react";
 import { Button } from "../../components/ui/button";
+import { Ghost } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export function Header() {
+  const router = useRouter();
   return (
     <header className="border-b">
       <div className="container mx-auto px-4 h-16 flex items-center justify-between">
-        <h1 className="text-2xl font-bold">InterviewIQ</h1>
+        <Button
+          style={{ marginRight: 10 }}
+          variant={"ghost"}
+          onClick={(event) => {
+            router.replace("/");
+          }}
+        >
+          <h1 className="text-2xl font-bold cursor-pointer">InterviewIQ</h1>
+        </Button>
         <Button
           variant="ghost"
           onClick={() => signOut({ callbackUrl: "/" })}
